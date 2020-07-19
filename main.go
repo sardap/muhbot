089939e61f1d1b93b5f2f719ffdb99b67f350b86
sardap <pfsarda23@gmail.com>
2020-07-19 14:12:42 +1000

    fixed case issue

--- main.go
+++ main.go
@@ -12,14 +12,14 @@ import (
 	"github.com/bwmarrin/discordgo"
 )
 
-var meRe *regexp.Regexp = regexp.MustCompile("\\b((me|ME)\\s*?)(\\r|\\n|\\.|$)")
+var meRe *regexp.Regexp = regexp.MustCompile("\\b((me)\\s*?)(\\r|\\n|\\.|$)")
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	if m.Author.ID == s.State.User.ID || m.Author.Bot {
 		return
 	}
 
-	if count := len(meRe.FindAllStringIndex(m.Content, -1)); count > 0 {
+	if count := len(meRe.FindAllStringIndex(strings.ToLower(m.Content), -1)); count > 0 {
 		var message strings.Builder
 		message.Grow(count*4 + len("<@>") + len(m.Author.ID) + 1)
 		fmt.Fprintf(&message, "<@%s> ", m.Author.ID)
