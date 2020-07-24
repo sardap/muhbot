14c0b4dce4f782d6c4dfd50c93baa31d3126c239
sardap <pfsarda23@gmail.com>
2020-07-24 12:58:15 +1000

    added stat tracking

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sardap/muhbot
 
 go 1.14
 
-require github.com/bwmarrin/discordgo v0.21.1
+require (
+	github.com/bwmarrin/discordgo v0.21.1
+	github.com/go-redis/redis v6.15.8+incompatible
+)
--- main.go
+++ main.go
@@ -6,20 +6,57 @@ import (
 	"os"
 	"os/signal"
 	"regexp"
+	"strconv"
 	"strings"
 	"syscall"
+	"time"
 	"unicode"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-redis/redis"
 )
 
-var meRe *regexp.Regexp = regexp.MustCompile("()(((me)\\.?(\\x60|_+|\\*+|\\*+_+|~~|\\|\\|)?)\\s*?)(\\r|\\n|\\.|$)")
+// NOTE commandRe is set in main
+var (
+	meRe      *regexp.Regexp
+	commandRe *regexp.Regexp
+	client    *redis.Client
+)
+
+func init() {
+	meRe = regexp.MustCompile("()(((me)\\.?(\\x60|_+|\\*+|\\*+_+|~~|\\|\\|)?)\\s*?)(\\r|\\n|\\.|$)")
+
+	redisAddress := os.Getenv("REDIS_ADDRESS")
+	dbNum, err := strconv.Atoi(os.Getenv("REDIS_DB"))
+	if err != nil {
+		panic(err)
+	}
+	password := os.Getenv("REDIS_PASSWORD")
+
+	tries := 0
+	for tries < 5 {
+		client = redis.NewClient(&redis.Options{
+			Addr:     redisAddress,
+			Password: password,
+			DB:       dbNum,
+		})
+		_, err := client.Ping().Result()
+		if err == nil {
+			break
+		}
+		time.Sleep(time.Duration(5) * time.Second)
+	}
+	if err != nil {
+		panic(err)
+	}
+}
 
 func reverse(s string) string {
 	runes := []rune(s)
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
+
 	return string(runes)
 }
 
@@ -29,7 +66,59 @@ func isUpper(s string) bool {
 			return false
 		}
 	}
+
 	return true
+}
+
+func userKey(gID, uID string) string {
+	return fmt.Sprintf("%s:%s", gID, uID)
+}
+
+func logMuh(gID, uID string, n int) {
+	client.IncrBy(userKey(gID, uID), int64(n))
+}
+
+func handleCommand(s *discordgo.Session, m *discordgo.MessageCreate) {
+	if regexp.MustCompile(".*?help$").Match([]byte(m.Content)) {
+		s.ChannelMessageSend(
+			m.ChannelID,
+			fmt.Sprintf(
+				"<@%s> Commands:\n"+
+					"\"<@%s> muh stats\" to show how many times you have said **the** word.",
+				m.Author.ID, s.State.User.ID,
+			),
+		)
+	} else if regexp.MustCompile(".*?muh.*?stats$").Match([]byte(m.Content)) {
+		res := client.Get(userKey(m.GuildID, m.Author.ID))
+		if res.Val() == "" {
+			s.ChannelMessageSend(
+				m.ChannelID,
+				fmt.Sprintf(
+					"<@%s> you have never **the** word since I started writing it down.",
+					m.Author.ID,
+				),
+			)
+			return
+		}
+
+		n, err := strconv.Atoi(res.Val())
+		if err != nil {
+			s.ChannelMessageSend(
+				m.ChannelID,
+				fmt.Sprintf("<@%s> Sever error tell paul.", m.Author.ID),
+			)
+			log.Printf("error getting data from DB %v\n", err)
+			return
+		}
+
+		s.ChannelMessageSend(
+			m.ChannelID,
+			fmt.Sprintf(
+				"<@%s> you have said **the** word %d times (since I started writing it down).",
+				m.Author.ID, n,
+			),
+		)
+	}
 }
 
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
@@ -38,10 +127,11 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	}
 
 	matches := meRe.FindAllStringSubmatchIndex(strings.ToLower(m.Content), -1)
-	count := len(matches)
-	if count > 0 {
+	if len(matches) > 0 {
 		var message strings.Builder
-		message.Grow(count*4 + len("<@>") + len(m.Author.ID) + 1)
+		go logMuh(m.GuildID, m.Author.ID, len(matches))
+
+		message.Grow(len(matches)*4 + len("<@>") + len(m.Author.ID) + 1)
 		fmt.Fprintf(&message, "<@%s> ", m.Author.ID)
 		for _, match := range matches {
 			modify := ""
@@ -69,7 +159,10 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 
 			fmt.Fprintf(&message, "%s%s%s ", modify, muhStr, reverse(modify))
 		}
+
 		s.ChannelMessageSend(m.ChannelID, message.String())
+	} else if commandRe.Match([]byte(m.Content)) {
+		handleCommand(s, m)
 	}
 }
 
@@ -90,6 +183,10 @@ func main() {
 		fmt.Println("error opening connection,", err)
 		return
 	}
+
+	commandRe = regexp.MustCompile(fmt.Sprintf("<@.%s>", discord.State.User.ID))
+
+	discord.UpdateStatus(1, "@me help")
 
 	// Wait here until CTRL-C or other term signal is received.
 	fmt.Println("Bot is now running.  Press CTRL-C to exit.")
